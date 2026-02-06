592e8c3ffdd13d8e5e3794d5557eb7aa6c6059c8
paper676 <paperr967@gmail.com>
2026-02-06 18:27:42 +0530

    Back/Arrow added at loging page

--- Quiztopia-FrontEnd/src/pages/Login.jsx
+++ Quiztopia-FrontEnd/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { UseAppContext } from '../context/AppContext';
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 import toast from 'react-hot-toast';
-
 function Login() {
     const { setUser, isUser, setIsUser, navigate, axios,user } = UseAppContext()
     const [state, setState] = React.useState("login");
@@ -31,38 +32,46 @@ function Login() {
         }
     }, [isUser]);
     return !user  && (
-        <div className='w-full lg:p-25 md:p-20 sm:p-5 flex justify-center align-center'>
-            <form onSubmit={onSubmitHandler} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-90 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
-                <p className="text-2xl font-medium m-auto">
-                    <span className="text-indigo-500"></span> {state === "login" ? "Login" : "Sign Up"}
-                </p>
-                {state === "register" && (
-                    <div className="w-full">
-                        <p>Name</p>
-                        <input onChange={(e) => setName(e.target.value)} value={name} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="text" required />
+        <div>
+            <div className='w-full pt-10 pl-10'>
+                <div className="flex flex-row items-center">
+                    <Link to='/' className="hover:scale-105 p-1 rounded-full transition hover:bg-zinc-100"><ArrowLeft size={18}/></Link>
+                    <p className='ml-1 text-sm text-black-600'>Home</p>
+                </div>
+            </div>
+            <div className='w-full lg:p-25 md:p-10 sm:p-5 flex justify-center align-center'>
+                <form onSubmit={onSubmitHandler} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-90 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
+                    <p className="text-2xl font-medium m-auto">
+                        <span className="text-indigo-500"></span> {state === "login" ? "Login" : "Sign Up"}
+                    </p>
+                    {state === "register" && (
+                        <div className="w-full">
+                            <p>Name</p>
+                            <input onChange={(e) => setName(e.target.value)} value={name} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="text" required />
+                        </div>
+                    )}
+                    <div className="w-full ">
+                        <p>Email</p>
+                        <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="email" required />
                     </div>
-                )}
-                <div className="w-full ">
-                    <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="email" required />
-                </div>
-                <div className="w-full ">
-                    <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="password" required />
-                </div>
-                {state === "register" ? (
-                    <p>
-                        Already have account? <span onClick={() => setState("login")} className="text-indigo-500 cursor-pointer">click here</span>
-                    </p>
-                ) : (
-                    <p>
-                        Create an account? <span onClick={() => setState("register")} className="text-indigo-500 cursor-pointer">click here</span>
-                    </p>
-                )}
-                <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
-                    {state === "register" ? "Create Account" : "Login"}
-                </button>
-            </form>
+                    <div className="w-full ">
+                        <p>Password</p>
+                        <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="password" required />
+                    </div>
+                    {state === "register" ? (
+                        <p>
+                            Already have account? <span onClick={() => setState("login")} className="text-indigo-500 cursor-pointer">click here</span>
+                        </p>
+                    ) : (
+                        <p>
+                            Create an account? <span onClick={() => setState("register")} className="text-indigo-500 cursor-pointer">click here</span>
+                        </p>
+                    )}
+                    <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+                        {state === "register" ? "Create Account" : "Login"}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
